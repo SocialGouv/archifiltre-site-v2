4af26600fbac6d273435d831a368f91dfb1dd30b
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2022-04-06 16:30:27 +0200

    :wrench: Remove robot header

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    async headers() {
-        return [
-            {
-                source: '/:path*',
-                headers: [
-                    {
-                        key: 'x-robots-tag',
-                        value: process.env.HEADER_ROBOTS_TAG,
-                    },
-                ],
-            },
-        ];
-    },
 };
 
 module.exports = nextConfig;
