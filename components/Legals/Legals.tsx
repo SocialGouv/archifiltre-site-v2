43f5a453fde8f9b41b47624d883ce3d4de0bd3e7
mehdilouraoui <37937348+mehdilouraoui@users.noreply.github.com>
2023-11-14 14:58:17 +0100

    feat/update (#160)
    * feat/update
    
    * feat/update

--- components/Legals/Legals.tsx
+++ components/Legals/Legals.tsx
@@ -1,52 +1,43 @@
 export const Legals = () => (
     <div className="legals">
-        <h1>Mentions légales</h1>
-        <h2 id="editeur-de-la-plateforme">Editeur de la plateforme</h2>
+        <h1>Mentions légales – Archifiltre</h1>
+
+        <h2>Éditeur de la plateforme</h2>
+        <ul>
+            <p>Direction du Numérique des ministères sociaux</p>
+            <p>Ministère des solidarités et de la santé</p>
+            <p>39-43 Quai André Citroën</p>
+            <p>75739 Paris Cedex 15</p>
+        </ul>
+        <h2>Directrice de la publication</h2>
         <p>
-            La Plateforme est éditée par la{' '}
-            <a href="https://incubateur.social.gouv.fr/">
-                Fabrique des Ministères sociaux
-            </a>{' '}
-            située :
+            Madame Anne JEANJEAN, Directrice du Numérique des ministères sociaux
         </p>
-        <p>Tour Mirabeau</p>
-        <p>39-43 Quai André Citroën</p>
-        <p>75015 PARIS</p>
-        <p>Tél : 01 40 56 60 00</p>
-        <h2 id="directeur-de-la-publication">Directeur de la publication</h2>
-        <p>Anne Jeanjean, Directrice du Numérique</p>
-        <h2 id="hébergement-de-la-plateforme">Hébergement de la plateforme</h2>
-        <p>Ce site est hébergé par :</p>
-        <p>Microsoft France</p>
-        <p>37 Quai du Président Roosevelt</p>
-        <p>92130 ISSY-LES-MOULINEAUX</p>
-        <h2 id="accessibilité">Accessibilité</h2>
+
+        <h2>Hébergement de la plateforme</h2>
+        <p>Microsoft Azure</p>
+
+        <p>37-39 Quai du Président Roosevelt</p>
+        <p>92130 Issy les Moulineaux</p>
+
+        <h2>Accessibilité</h2>
+
         <p>
             La conformité aux normes d’accessibilité numérique est un objectif
-            ultérieur mais nous tâchons de rendre ce site accessible à toutes et
-            à tous.
+            ultérieur mais nous tâchons de rendre cette plateforme accessible à
+            toutes et tous.
         </p>
+
         <h2>Signaler un dysfonctionnement</h2>
         <p>
             Si vous rencontrez un défaut d’accessibilité vous empêchant
-            d’accéder à un contenu ou une fonctionnalité du site, merci de nous
-            en faire part. Si vous n’obtenez pas de réponse rapide de notre
-            part, vous êtes en droit de faire parvenir vos doléances ou une
-            demande de saisine au Défenseur des droits.
+            d’accéder à un contenu ou une fonctionnalité de la plateforme, merci
+            de nous en faire part.
         </p>
-        <h2>En savoir plus</h2>
         <p>
-            Pour en savoir plus sur la politique d’accessibilité numérique de
-            l’État :{' '}
-            <a href="http://references.modernisation.gouv.fr/accessibilite-numerique">
-                http://references.modernisation.gouv.fr/accessibilite-numerique
-            </a>
-        </p>
-        <h2 id="sécurité">Sécurité</h2>
-        <p>
-            Le site est protégé par un certificat électronique, matérialisé pour
-            la grande majorité des navigateurs par un cadenas. Cette protection
-            participe à la confidentialité des échanges.
+            Si vous n’obtenez pas de réponse rapide de notre part, vous êtes en
+            droit de faire parvenir vos doléances ou une demande de saisine au
+            Défenseur des droits.
         </p>
     </div>
 );
