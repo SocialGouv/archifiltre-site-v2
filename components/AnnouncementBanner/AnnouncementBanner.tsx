994945c9236069c09dbe2ee0891a109f92deebe0
Timothée Malard <timothee.malard.pro@gmail.com>
2024-03-14 13:07:21 +0100

    feat: improve display & add AnnouncementBanner

---
+++ components/AnnouncementBanner/AnnouncementBanner.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import React from 'react';
+import { getDownloadLink } from '../../utils';
+
+const AnnouncementBanner = () => {
+    const docsDownloadLink = getDownloadLink(undefined, 'docs');
+
+    return (
+        <div className="announcement-banner">
+            Nouvelle version disponible :{' '}
+            <strong>
+                Archifiltre Docs v4.1.0 &quot;Unicorn Utopia&quot; 🦄
+            </strong>{' '}
+            {docsDownloadLink && (
+                <Link href={docsDownloadLink} className={'announcement-link'}>
+                    Découvrez-la ici
+                </Link>
+            )}
+        </div>
+    );
+};
+
+export default AnnouncementBanner;
