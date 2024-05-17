b291d500c441c1a6d0ef2cd70ed4d7972fcbf280
Timothée Malard <timothee.malard.pro@gmail.com>
2024-05-17 21:49:13 +0900

    set archifiltre-docs to v4.1.1 (#172)

--- utils/downloadLink.ts
+++ utils/downloadLink.ts
@@ -1,4 +1,4 @@
 export const BASE_URL_MAILS =
     'https://github.com/SocialGouv/archifiltre-mails/releases/download/v1.0.0/archifiltre-mails';
 export const BASE_URLS_DOCS =
-    'https://github.com/SocialGouv/archifiltre-docs/releases/download/v4.1.0/archifiltre-docs';
+    'https://github.com/SocialGouv/archifiltre-docs/releases/download/v4.1.1/archifiltre-docs';
