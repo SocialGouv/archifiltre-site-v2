1b56a545a1872c52f0fbcbec2e95c2fa5520c247
Samuel Pirès <samuel.pires.pro@gmail.com>
2024-06-13 17:58:50 +0200

    Update Legals.tsx
    Mise à jour des mentions légales

--- components/Legals/Legals.tsx
+++ components/Legals/Legals.tsx
@@ -3,41 +3,66 @@ export const Legals = () => (
         <h1>Mentions légales – Archifiltre</h1>
 
         <h2>Éditeur de la plateforme</h2>
-        <ul>
-            <p>Direction du Numérique des ministères sociaux</p>
-            <p>Ministère des solidarités et de la santé</p>
-            <p>39-43 Quai André Citroën</p>
-            <p>75739 Paris Cedex 15</p>
-        </ul>
+        <p>
+            La plateforme « Archifiltre » est éditée au sein de la Fabrique numérique des ministères sociaux, située :
+        </p>
+        <p>
+            Tour Mirabeau
+        </p>
+        <p>
+            39-43 Quai André Citroën
+        </p>
+        <p>
+            75739 Paris Cedex 15
+        </p>
+        <p>
+            France
+        </p>
+        <p>
+            Téléphone : 01 44 38 36 02
+        </p>
+        
+
+        
         <h2>Directrice de la publication</h2>
         <p>
-            Madame Anne JEANJEAN, Directrice du Numérique des ministères sociaux
+            La directrice de publication est Madame Anne JEANJEAN, directrice du numérique des ministères sociaux.
         </p>
 
         <h2>Hébergement de la plateforme</h2>
-        <p>Microsoft Azure</p>
-
-        <p>37-39 Quai du Président Roosevelt</p>
-        <p>92130 Issy les Moulineaux</p>
+        <p>
+            OVH SAS
+        </p>
+        <p>
+            2 rue Kellermann
+        </p>
+        <p>
+            59100 Roubaix
+        </p>
+        <p>
+            France
+        </p>
+        
 
         <h2>Accessibilité</h2>
 
         <p>
-            La conformité aux normes d’accessibilité numérique est un objectif
-            ultérieur mais nous tâchons de rendre cette plateforme accessible à
-            toutes et tous.
+            La conformité aux normes d'accessibilité numérique est un objectif ultérieur mais nous tâchons de rendre cette plateforme accessible à toutes et à tous.
         </p>
 
-        <h2>Signaler un dysfonctionnement</h2>
+        <h2>En savoir plus</h2>
         <p>
-            Si vous rencontrez un défaut d’accessibilité vous empêchant
-            d’accéder à un contenu ou une fonctionnalité de la plateforme, merci
-            de nous en faire part.
+            Pour en savoir plus sur la politique d’accessibilité numérique de l’État : <a href="url">https://accessibilite.numerique.gouv.fr/</a>.
+        </p>
+
+        <h2>Sécurité</h2>
+        <p>
+            La plateforme est protégée par un certificat électronique, matérialisé pour la grande majorité des navigateurs par un cadenas. Cette protection participe à la confidentialité des échanges.
         </p>
         <p>
-            Si vous n’obtenez pas de réponse rapide de notre part, vous êtes en
-            droit de faire parvenir vos doléances ou une demande de saisine au
-            Défenseur des droits.
+            En aucun cas, les services associés à la plateforme ne seront à l’origine d’envoi de courriels pour demander la saisie d’informations personnelles.
         </p>
+
+        
     </div>
 );
