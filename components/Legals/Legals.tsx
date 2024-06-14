fd6cd60381e751c7963b701aa977a63d884350ec
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2024-06-14 16:29:28 +0200

    fix: prettier errors

--- components/Legals/Legals.tsx
+++ components/Legals/Legals.tsx
@@ -4,65 +4,55 @@ export const Legals = () => (
 
         <h2>Éditeur de la plateforme</h2>
         <p>
-            La plateforme « Archifiltre » est éditée au sein de la Fabrique numérique des ministères sociaux, située :
+            La plateforme « Archifiltre » est éditée au sein de la Fabrique
+            numérique des ministères sociaux, située :
         </p>
-        <p>
-            Tour Mirabeau
-        </p>
-        <p>
-            39-43 Quai André Citroën
-        </p>
-        <p>
-            75739 Paris Cedex 15
-        </p>
-        <p>
-            France
-        </p>
-        <p>
-            Téléphone : 01 44 38 36 02
-        </p>
-        
+        <p>Tour Mirabeau</p>
+        <p>39-43 Quai André Citroën</p>
+        <p>75739 Paris Cedex 15</p>
+        <p>France</p>
+        <p>Téléphone : 01 44 38 36 02</p>
 
-        
         <h2>Directrice de la publication</h2>
         <p>
-            La directrice de publication est Madame Anne JEANJEAN, directrice du numérique des ministères sociaux.
+            La directrice de publication est Madame Anne JEANJEAN, directrice du
+            numérique des ministères sociaux.
         </p>
 
         <h2>Hébergement de la plateforme</h2>
-        <p>
-            OVH SAS
-        </p>
-        <p>
-            2 rue Kellermann
-        </p>
-        <p>
-            59100 Roubaix
-        </p>
-        <p>
-            France
-        </p>
-        
+        <p>OVH SAS</p>
+        <p>2 rue Kellermann</p>
+        <p>59100 Roubaix</p>
+        <p>France</p>
 
         <h2>Accessibilité</h2>
 
         <p>
-            La conformité aux normes d'accessibilité numérique est un objectif ultérieur mais nous tâchons de rendre cette plateforme accessible à toutes et à tous.
+            La conformité aux normes d&apos;accessibilité numérique est un
+            objectif ultérieur mais nous tâchons de rendre cette plateforme
+            accessible à toutes et à tous.
         </p>
 
         <h2>En savoir plus</h2>
         <p>
-            Pour en savoir plus sur la politique d’accessibilité numérique de l’État : <a href="https://accessibilite.numerique.gouv.fr/">https://accessibilite.numerique.gouv.fr/</a>.
+            Pour en savoir plus sur la politique d&apos;accessibilité numérique
+            de l&apos;État :{' '}
+            <a href="https://accessibilite.numerique.gouv.fr/">
+                https://accessibilite.numerique.gouv.fr/
+            </a>
+            .
         </p>
 
         <h2>Sécurité</h2>
         <p>
-            La plateforme est protégée par un certificat électronique, matérialisé pour la grande majorité des navigateurs par un cadenas. Cette protection participe à la confidentialité des échanges.
+            La plateforme est protégée par un certificat électronique,
+            matérialisé pour la grande majorité des navigateurs par un cadenas.
+            Cette protection participe à la confidentialité des échanges.
         </p>
         <p>
-            En aucun cas, les services associés à la plateforme ne seront à l’origine d’envoi de courriels pour demander la saisie d’informations personnelles.
+            En aucun cas, les services associés à la plateforme ne seront à
+            l&apos;origine d&apos;envoi de courriels pour demander la saisie
+            d&apos;informations personnelles.
         </p>
-
-        
     </div>
 );
--- components/Policy/Policy.tsx
+++ components/Policy/Policy.tsx
@@ -4,44 +4,58 @@ export const Policy = () => (
         <h1>Politique de confidentialité – Archifiltre</h1>
 
         <p>
-            Archifiltre est à l’initiative de la Direction des finances, achats
-            et des services au sein de la Fabrique numérique des ministères
-            sociaux.
+            Archifiltre est à l&apos;initiative de la Direction des finances,
+            achats et des services au sein de la Fabrique numérique des
+            ministères sociaux.
         </p>
-        <h2>Quel est l’objectif d’Archifiltre ?</h2>
+        <h2>Quel est l&apos;objectif d&apos;Archifiltre ?</h2>
         <p>
-            Proposer aux utilisateurs de fichiers bureautiques un outil de visualisation d'arborescences pour analyse, audit, tri des documents sur lesquels ils travaillent au quotidien dans les domaines variés constitutifs de leurs missions, que ces documents comportent ou non des données à caractère personnel.  Les fichiers (documents de travail de l'utilisateur notamment) sont in fine versés dans un système d'archivage électronique.
+            Proposer aux utilisateurs de fichiers bureautiques un outil de
+            visualisation d'arborescences pour analyse, audit, tri des documents
+            sur lesquels ils travaillent au quotidien dans les domaines variés
+            constitutifs de leurs missions, que ces documents comportent ou non
+            des données à caractère personnel. Les fichiers (documents de
+            travail de l'utilisateur notamment) sont in fine versés dans un
+            système d'archivage électronique.
         </p>
 
         <h2>Confidentialité</h2>
         <p>
-            Aucune donnée à caractère personnel n’est traitée ou remontée.
-            Toutes les données sont maintenues dans l’appareil en local de
-            l’utilisateur. Par ailleurs, nous ne sommes pas en mesure
-            d’identifier les personnes concernées.
+            Aucune donnée à caractère personnel n&apos;est traitée ou remontée.
+            Toutes les données sont maintenues dans l&apos;appareil en local de
+            l&apos;utilisateur. Par ailleurs, nous ne sommes pas en mesure
+            d&apos;identifier les personnes concernées.
         </p>
 
         <h2>Sous-traitants</h2>
 
         <table>
-          <tr>
-            <th>Sous-traitant</th>
-            <th>Pays destinataire</th>
-            <th>Traitement réalisé</th>
-            <th>Garanties</th>
-          </tr>
-          <tr>
-            <td>OVH</td>
-            <td>France</td>
-            <td>Hébergement</td>
-            <td><a href="https://us.ovhcloud.com/legal/data-processing-agreement/">https://us.ovhcloud.com/legal/data-processing-agreement/</a></td>
-          </tr>
-          <tr>
-            <td>Tally</td>
-            <td>Belgique</td>
-            <td>Hébergement des données de questionnaires</td>
-            <td><a href="https://nextcloud.fabrique.social.gouv.fr/s/bPtSaxaL8cP9ymx">https://nextcloud.fabrique.social.gouv.fr/s/bPtSaxaL8cP9ymx</a></td>
-          </tr>
-        </table>        
+            <tr>
+                <th>Sous-traitant</th>
+                <th>Pays destinataire</th>
+                <th>Traitement réalisé</th>
+                <th>Garanties</th>
+            </tr>
+            <tr>
+                <td>OVH</td>
+                <td>France</td>
+                <td>Hébergement</td>
+                <td>
+                    <a href="https://us.ovhcloud.com/legal/data-processing-agreement/">
+                        https://us.ovhcloud.com/legal/data-processing-agreement/
+                    </a>
+                </td>
+            </tr>
+            <tr>
+                <td>Tally</td>
+                <td>Belgique</td>
+                <td>Hébergement des données de questionnaires</td>
+                <td>
+                    <a href="https://nextcloud.fabrique.social.gouv.fr/s/bPtSaxaL8cP9ymx">
+                        https://nextcloud.fabrique.social.gouv.fr/s/bPtSaxaL8cP9ymx
+                    </a>
+                </td>
+            </tr>
+        </table>
     </div>
 );
